19d7b445ebb93134857d37db1375cda1369d96e9
Luis Manzanillas <lmanzanillas@gmail.com>
2024-10-15 15:19:58 +0200

    add G4_connecto file to put geant4 related staff

---
+++ src/G4_connector.jl
@@ -0,0 +1,27 @@
+"""
+function get_evts_index(df::DataFrame)
+Function to get the index fo start and end of each event in a ulalap.csv file
+It accepts a dataframe and returns a matrix with column corresponding to the: number_of_event  index_start_evt  index_ends_evt
+"""
+function get_evts_index(df::DataFrame)
+    event_number = df[1,:evt]
+    evt_counter = 1
+    evts_info = []
+    start_evt_index = 1
+    for i = 1 : 1 : length(df[!,:evt])
+        if  event_number == df[i,:evt]
+            continue
+        else
+            end_evt_index = i - 1
+            push!(evts_info,[event_number start_evt_index end_evt_index])
+            event_number = df[i,:evt]
+            evt_counter += 1
+            start_evt_index = i
+        end
+    end
+    #evt_counter += 1
+    start_evt_index = end_evt_index + 1
+    end_evt_index = length(df[!,:evt])
+    push!(evts_info,[event_number start_evt_index end_evt_index])
+    return vcat(evts_info...)
+end
