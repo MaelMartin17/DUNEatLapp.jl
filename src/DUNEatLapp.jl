459239d36231cd35f8c731c08cf7f08cf081633b
MaelMartin17 <mael.martin@lapp.in2p3.fr>
2024-12-05 16:37:32 +0100

    add analysis lardon

--- src/DUNEatLapp.jl
+++ src/DUNEatLapp.jl
@@ -23,9 +23,11 @@ export get_clusters_energy_of_evt
 export process_clustering_neutron_file
 export get_capture_position
 export is_n_capture_on_Ar
+export Single_Hits_lardon
 include("functions.jl")
 include("clustering.jl")
 include("G4_connector.jl")
 include("neutron_capture_info.jl")
+include("analyse_lardon.jl")
 
 end
