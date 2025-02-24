86e51f5d490cbad2fda3f5329ce6b766a144299d
MaelMartin17 <mael.martin@lapp.in2p3.fr>
2025-02-24 17:17:59 +0100

    add apa singles_hits_lardon

--- src/analyse_lardon.jl
+++ src/analyse_lardon.jl
@@ -4,7 +4,7 @@ function to collect the data about the single hits found by Lardon.
 It receives the path of the HDF5 files in your directory and it returns a DataFrame with all the single Hits and the information about
 """
 function Single_Hits_lardon(path::String)
-    Info = DataFrame(Id=Int32[],x=Float32[],y=Float32[],z=Float32[],t=Float32[],tdc_max=Vector[],tdc_min=Vector[],charge_neg=Vector[],charge_pos=Vector[],d_bary_max=Float32[],d_track_2D=Float32[],veto=Vector[],fc_max=Float32[],tdc_start=Vector[],tdc_stop=Vector[]);
+    Info = DataFrame(Id=Int32[],apa=Int32, x=Float32[],y=Float32[],z=Float32[],t=Float32[],tdc_max=Vector[],tdc_min=Vector[],charge_neg=Vector[],charge_pos=Vector[],d_bary_max=Float32[],d_track_2D=Float32[],veto=Vector[],fc_max=Float32[],tdc_start=Vector[],tdc_stop=Vector[]);
     for (root, dirs, files) in walkdir(path)
         for i in ProgressBar(files)
             fid = h5open("$(root)/$(i)", "r")
@@ -13,7 +13,7 @@ function Single_Hits_lardon(path::String)
             Hset = fid["hits"]
             B = read(Hset);            
             for i in 1:1:length(A)
-                push!(Info,[A[i].ID, A[i].x, A[i].y, A[i].z, A[i].timestamp, A[i].tdc_max, A[i].tdc_min, A[i].charge_neg, A[i].charge_pos, A[i].d_bary_max, A[i].d_track_2D, A[i].veto,B[(A[i].ID)+1].fC_max,A[i].tdc_start,A[i].tdc_stop]) 
+                push!(Info,[A[i].ID, A[i].module,A[i].x, A[i].y, A[i].z, A[i].timestamp, A[i].tdc_max, A[i].tdc_min, A[i].charge_neg, A[i].charge_pos, A[i].d_bary_max, A[i].d_track_2D, A[i].veto,B[(A[i].ID)+1].fC_max,A[i].tdc_start,A[i].tdc_stop]) 
             end    
         end
     end
